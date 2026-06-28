a2433fce4d370b3b5ecc6444e0eedd15ee0d033a
Geya Malini Uppalapati <your-github-email@example.com>
2026-06-28 15:18:13 +0530

    Deploy

--- app.py
+++ app.py
@@ -166,6 +166,8 @@ def download_report():
     return send_file(PDF_REPORT, as_attachment=True)
 
 # --------------------- Main ---------------------------
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
-    
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
